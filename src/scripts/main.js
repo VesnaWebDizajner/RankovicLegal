fc2f30d741f284c415ded4d51d87de1612ac20a8
Krivobarac <gasparovicsasa@yahoo.com>
2025-10-09 18:02:05 +0200

    Contact form section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
 import './components/practice-section-tile-component.js';
 import './components/practice-section-component.js';
+import './components/contact-section-component.js';
 import './components/footer-component.js';
