2ba4b92b9dd6df8d07c4cbe5cfeb5ebde5a3ef3e
Krivobarac <gasparovicsasa@yahoo.com>
2025-10-14 18:15:22 +0200

    navigation layout  place replacing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,62 +6,34 @@ import "./components/footer-section-component.js";
 import "./components/counters-component.js";
 import "./components/top-bar-component.js";
 
-// // Function to toggle the background color visibility of the navigation bar on scrolling
-// (function () {
-//   const nav = document.querySelector(".navigation-section");
-//   const className = "scrolled";
+// Function to toggle the background color visibility of the navigation bar on scrolling
+(function () {
+  const nav = document.querySelector(".navigation-section");
+  const className = "scrolled";
 
-//   document.addEventListener("scroll", () => {
-//     if (window.scrollY > 50) {
-//       nav.classList.add(className);
-//     } else {
-//       nav.classList.remove(className);
-//     }
-//   });
-// })();
-
-// // Function to hide the navigation menu when the user scrolls or resizes the window
-// (function () {
-//   const navToogler = document.querySelector("#nav-toggle");
-
-//   navToogler.addEventListener("change", () => {
-//     if (navToogler.checked) {
-//       const uncheckNavToggler = () => {
-//         navToogler.checked = false;
-//         window.removeEventListener("scroll", uncheckNavToggler);
-//         window.removeEventListener("resize", uncheckNavToggler);
-//       };
-
-//       window.addEventListener("scroll", uncheckNavToggler);
-//       window.addEventListener("resize", uncheckNavToggler);
-//     }
-//   });
-// })();
-
-// Hamburger ikona na responsive dizajnu sajta
-
-const hamburger = document.getElementById("hamburger");
-const navWrapper = document.querySelector(".nav_wrapper");
-
-hamburger.addEventListener("click", () => {
-  navWrapper.classList.toggle("open");
-});
-
-document.querySelectorAll(".nav_link").forEach((link) => {
-  link.addEventListener("click", () => {
-    navWrapper.classList.remove("open");
-  });
-});
-
-// Kada se skroluje ekran nav sekcija dobija tamnu pozadinu i ostaje na vrhu ekrana
-
-document.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("scroll", function () {
-    const nav = document.querySelector(".nav");
-    if (window.scrollY > 50) {
-      nav.classList.add("scrolled");
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > 40) {
+      nav.classList.add(className);
     } else {
-      nav.classList.remove("scrolled");
+      nav.classList.remove(className);
     }
   });
-});
+})();
+
+// Function to hide the navigation menu when the user scrolls or resizes the window
+(function () {
+  const navToogler = document.querySelector("#nav-toggle");
+
+  navToogler.addEventListener("change", () => {
+    if (navToogler.checked) {
+      const uncheckNavToggler = () => {
+        navToogler.checked = false;
+        window.removeEventListener("scroll", uncheckNavToggler);
+        window.removeEventListener("resize", uncheckNavToggler);
+      };
+
+      window.addEventListener("scroll", uncheckNavToggler);
+      window.addEventListener("resize", uncheckNavToggler);
+    }
+  });
+})();
