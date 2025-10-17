7755763dcf3f355109cd5d14b8a648bba4f7cdbe
Krivobarac <gasparovicsasa@yahoo.com>
2025-10-17 16:07:39 +0200

    law-help section as component

---
+++ src/scripts/components/law-help-section-component.js
@@ -0,0 +1,26 @@
+class LawHelpSectionComponent extends HTMLElement {
+    connectedCallback() {
+        const elements = this.innerHTML;
+        this.innerHTML = `
+                <div class="law-help__content container-l">
+                    <div class="law-help__content-container">
+                        <h1 class="law-help__content-title">Potrebna Vam je pomoć u prekršajnim postupcima? </h1>
+                        <p class="law-help__content-text">
+                            Advokatska kancelarija Ranković pruža sveobuhvatne usluge i iz oblasti
+                            <i>prekršajnog prava</i>.
+                        </p>
+                        <a href="#kontakt-forma" class="bttn-primary-m">Zakaži konsultacije</a>
+                    </div>
+                    <div class="law-help__content-img">
+                        <img src="../src/assets/img/Tomas Plastic IG Story 1.png" alt="criminal-law-picture">
+                    </div>
+                </div>`;
+                    
+        this.style.display = "block";
+        this.classList.add("law-help");
+        this.role = "region";
+        this.ariaLabel = "Sekcija Pravna pomoć"
+    }
+}
+
+customElements.define("law-help-section-component", LawHelpSectionComponent);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,7 @@ import "./components/top-bar-component.js";
 import "./components/law-aria-section-component.js";
 import "./components/law-aria-navigation-component.js";
 import "./components/law-area-aside-component.js";
+import "./components/law-help-section-component.js";
 
 // Function to toggle the background color visibility of the navigation bar on scrolling
 (function () {
